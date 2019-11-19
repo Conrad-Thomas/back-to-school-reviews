bd68f88f6d495e457f48d1990111f8b878b28fef
Conrad-Thomas <thomcs4@student.op.ac.nz>
2019-11-19 14:45:42 +1300

    Updated nav bar placement

--- public/js/contact.js
+++ public/js/contact.js
@@ -1,12 +1,12 @@
-let contactForm = document.getElementById('contact-form')
-let name = document.getElementById('contact-name')
-let output = document.getElementById('get-in-touch')
-let replace = document.querySelector('.contact')
+
 
 //method that sends an alert with a message and grabs 
 //the value of the name.
 
-$( "#contact-form" ).submit(function( event ) {
-    replace.innerHTML = `<h1 id="page-header2">Thank you ${name.value}, we will get in touch soon!</h1>`
-    event.preventDefault()
-});
+$( document ).ready(function() {
+    let name = document.getElementById('contact-name')
+    $( '#contact-form' ).submit(function( event ) {
+        event.preventDefault()
+        $( '.contact' ).html(`<h1 id="page-header2">Thank you ${name.value}, we will get in touch soon!</h1>`)
+    });
+})
