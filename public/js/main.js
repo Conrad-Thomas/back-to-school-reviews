16f46f26f8cdcc5db1317ac9f50fca7695d7c1b1
Conrad Thomas <thomcs4@student.op.ac.nz>
2019-11-21 09:36:07 +1300

    Cleaned up javascript files

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,29 @@
-const commentForm = document.getElementById('#comment-form')
+//contact jquery code.
+$( document ).ready(function() {
+    let name = document.getElementById('contact-name')
+    $( '#contact-form' ).submit(function( event ) {
+        event.preventDefault()
+        $( '.contact' ).html(`<h1 id="page-header2">Thank you ${name.value}, we will get in touch soon!</h1>`)
+    });
+})
+
+
+//starts jquery code
+$(function () {
+ 
+    var $rateYo = $(".rateYo").rateYo({"rating": 0.7});
+   
+    /* set the option `onChange` */
+    $rateYo.rateYo("option", "onChange", function () {
+        $(this).next().text(rating);
+    });
+   
+    /* set the option `multiColor` to show Multi Color Rating */
+    $rateYo.rateYo("option", "multiColor", true);
+  });
+
+
+  const commentForm = document.getElementById('#comment-form')
 const commentOutput = document.getElementById('comment-data')
 
 
